497b2ab87c56e4e75d27ca1cb341ed55b3f9e329
edmariooliver <edmariooliver@Outlook.com>
2024-07-12 22:23:04 -0300

    rabisco de chat

--- websocket.php
+++ websocket.php
@@ -1,22 +1,89 @@
 <?php
 
-$ws = new Swoole\WebSocket\Server('0.0.0.0', 8000);
+use Swoole\WebSocket\Server;
+use Swoole\WebSocket\Frame;
+use Swoole\Http\Request;
 
-$ws->on('Open', function ($ws, $request) {
-    $ws->push($request->fd, "hello, welcome\n");
-});
+class Chat
+{
+    protected $clients;
+    protected $messagesCount;
 
-$ws->on('Message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
-    foreach ($ws->connections as $fd) {
-        if ($frame->fd != $fd) {
-            $ws->push($fd, "client-{$frame->fd}: {$frame->data}");
+    public function __construct() {
+        $this->messagesCount = 0;
+        $this->clients = [];
+    }
+
+    public function onOpen(Server $server, Request $request) {
+        $fd = $request->fd;
+        $this->clients[$fd] = $request;
+        
+        $name = $request->header['cookie'] ?? "Anonimo{$fd}";
+        $name = explode("@", $name);
+        $name = isset($name[1]) ? $name[1] : $name[0];
+
+
+        echo "New connection! ({$name})\n";
+    }
+
+    public function onMessage(Server $server, Frame $frame) {
+        try {
+            exec("php Hook.php");
+            $msg = json_decode($frame->data, true);
+
+            $name = $msg['name'] ?? "Anonimo{$frame->fd}";
+            $msg = [
+                "nome" => $name,
+                "msg" => trim($msg['msg'])
+            ];
+
+            if (strlen($msg['msg']) > 100) {
+                $msg['msg'] = "(Esta mensagem está indisponível)";
+                $msg['type'] = "error";
+            }
+            if (strlen($msg['nome']) > 100) {
+                $msg['nome'] = "Anonimo{$frame->fd}";
+            }
+            if (strlen($msg['msg']) === 0) {
+                $msg['msg'] = "(Esta mensagem está indisponível)";
+                $msg['type'] = "error";
+            }
+
+            foreach ($this->clients as $clientFd => $clientRequest) {
+                if ($frame->fd !== $clientFd) {
+                    $server->push($clientFd, json_encode($msg));
+                }
+            }
+
+            foreach ($this->clients as $clientFd => $clientRequest) {
+                $server->push($clientFd, json_encode($msg));
+            }
+        } catch (\Throwable $th) {
+            // Handle error
         }
     }
-});
 
-$ws->on('Close', function ($ws, $fd) {
-    echo "client-{$fd} is closed\n";
-});
+    public function onClose(Server $server, int $fd) {
+        unset($this->clients[$fd]);
 
-$ws->start();
+        $name = $this->clients[$fd]->header['cookie'] ?? "Anonimo{$fd}";
+        $name = explode("@", $name);
+        $name = isset($name[1]) ? $name[1] : $name[0];
+        echo "Connection {$fd} has disconnected\n";
+    }
+
+    public function onError(Server $server, int $fd, \Throwable $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $server->close($fd);
+    }
+}
+
+$server = new Server('0.0.0.0', 8000);
+$chat = new Chat();
+
+$server->on('Open', [$chat, 'onOpen']);
+$server->on('Message', [$chat, 'onMessage']);
+$server->on('Close', [$chat, 'onClose']);
+$server->on('Error', [$chat, 'onError']);
+
+$server->start();
